68f8c53b8f777693d59157b9904666c6c2187478
Electrostats <Super_justy@hotmail.com>
2024-01-07 18:02:23 -0500

    Simplified everything

--- Plugin.cs
+++ Plugin.cs
@@ -24,59 +24,25 @@ public class Plugin : BaseUnityPlugin
         [HarmonyPatch(nameof(HomeController.loadscene))]
         public static bool Prefix(HomeController __instance)
         {
-            LeanTween.cancelAll();
             if (__instance.loadsceneindex == 0)
             {
+                LeanTween.cancelAll();
                 GlobalVariables.scene_destination = "levelselect";
                 SceneManager.LoadScene("levelselect");
+                return false;
             }
-            else if (__instance.loadsceneindex == 1)
-            {
-                GlobalVariables.scene_destination = "levelselect";
-                SceneManager.LoadScene("charselect_mp");
-            }
-            else if (__instance.loadsceneindex == 3)
-            {
-                GlobalVariables.tootscene = "treble";
-                SceneManager.LoadScene("tootvessel");
-            }
-            else if (__instance.loadsceneindex == 4)
-            {
-                GlobalVariables.scene_destination = "freeplay";
-                GlobalVariables.chosen_track = "freeplay";
-                SceneManager.LoadScene("gameplay");
-            }
-            else if (__instance.loadsceneindex == 6)
-                SceneManager.LoadScene("cards");
-            else
-                SceneManager.LoadScene(__instance.loadsceneindex);
-            return false;
+            return true;
         }
     }
 
     [HarmonyPatch(typeof(LevelSelectController))]
     public static class LevelSelectControllerPatches
     {
-        [HarmonyPatch(nameof(LevelSelectController.clickBack))]
-        public static bool Prefix(LevelSelectController __instance)
+        [HarmonyPatch(nameof(LevelSelectController.fadeOut))]
+        public static void Postfix(string scene_name, LevelSelectController __instance)
         {
-            if (__instance.back_clicked || __instance.btntimer > 0.0)
-                return false;
-            __instance.clipPlayer.cancelCrossfades();
-            GlobalVariables.levelselect_index = __instance.songindex;
-            GlobalVariables.practicemode = 1f;
-            GlobalVariables.turbomode = false;
-            __instance.back_clicked = true;
-            __instance.bgmus.Stop();
-            __instance.doSfx(__instance.sfx_slidedown);
-            if (GlobalVariables.numplayers == 1)
-            {
-                __instance.fadeOut("home", 0.35f);
-            }
-            else if (GlobalVariables.numplayers > 1) {
-                __instance.fadeOut("charselect_mp", 0.35f);
-            }
-            return false;
+            if (scene_name == "charselect")
+                __instance.nextscene = "home";
         }
     }
 }
